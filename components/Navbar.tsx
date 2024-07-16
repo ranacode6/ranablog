0fa17950cb62ea548295208b71c1765a89702539
rana <rana.ahammed.012@gmail.com>
2024-07-16 20:55:44 +0600

    changed domain name

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <nav className="w-full relative flex items-center justify-between max-w-2xl mx-auto px-4 py-5">
       <Link href="/" className="font-bold text-3xl">
-        Rana<span className="text-primary">Vlog</span>
+        Rana<span className="text-primary">Blog</span>
       </Link>
       <ModeToggle />
     </nav>
